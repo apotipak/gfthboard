c9f10361d9406706478a8158bdff533002f92d0e
gfthamnaj <60995426+gfthamnaj@users.noreply.github.com>
2020-02-28 10:47:02 +0700

    Dev (#15)
    * Add counting number
    
    * Fixed employee menu.
    
    * Add Shift Type column
    
    * Add start date.
    
    * Add initial files.
    
    * Add initial files.
    
    * Change app icon color.
    
    * Fix modal dialog height
    
    * Change label
    
    * Fix typo
    
    * Add initial files.
    
    Co-authored-by: apotipak <apotipak@gmail.com>

--- leave/urls.py
+++ leave/urls.py
@@ -7,13 +7,15 @@ urlpatterns = [
     path('leave-history/', views.EmployeeInstanceListView.as_view(), name='leave_history'),
     path('leave-history/<int:pk>', views.EmployeeInstanceDetailView.as_view(), name='leave_history_detail'),
     path('leave-history/<uuid:pk>/delete/', views.EmployeeInstanceDelete.as_view(), name='leave_history_delete'),
-    
+]
+
+urlpatterns += [
     path('leave-approval/', views.LeaveApprovalListView.as_view(), name='leave_approval'),
     path('leave-approval/<uuid:pk>/approve/', views.EmployeeInstanceApprove, name='leave_approve'),
     path('leave-approval/<uuid:pk>/reject/', views.EmployeeInstanceReject, name='leave_reject'),
 ]
 
 urlpatterns += [
-    path('employee/create/', views.employee_new, name='employee_create'),
+    path('employee/create/', views.EmployeeNew, name='employee_create'),
 ]
 
--- page/views.py
+++ page/views.py
@@ -6,16 +6,38 @@ from leave.models import LeaveEmployee
 
 @login_required(login_url='/accounts/login/')
 def index(request):
-    return render(request, 'index.html', {})
+	page_title = settings.PROJECT_NAME
+	db_server = settings.DATABASES['default']['HOST']
+	project_name = settings.PROJECT_NAME
+	project_version = settings.PROJECT_VERSION
+	today_date = settings.TODAY_DATE
+
+	return render(request, 'index.html', {
+        'page_title': page_title, 
+        'project_name': project_name, 
+        'project_version': project_version, 
+        'db_server': db_server, 'today_date': today_date
+    })
+
+
 
 @login_required(login_url='/accounts/login/')
 def StaffProfile(request):
+    page_title = settings.PROJECT_NAME
+    db_server = settings.DATABASES['default']['HOST']
+    project_name = settings.PROJECT_NAME
+    project_version = settings.PROJECT_VERSION
+    today_date = settings.TODAY_DATE    
     EmployeeInstance = LeaveEmployee.EmployeeInstance(request)
     SuperVisorInstance = LeaveEmployee.SuperVisorInstance(request)
     TeamMemberList = LeaveEmployee.TeamMemberList(request)
 
     return render(request, 'page/staff_profile.html', {
+        'page_title': page_title, 
+        'project_name': project_name, 
+        'project_version': project_version, 
+        'db_server': db_server, 'today_date': today_date,
         'EmployeeInstance': EmployeeInstance,
         'SuperVisorInstance': SuperVisorInstance,
         'TeamMemberList': TeamMemberList,
-        })
+    })
