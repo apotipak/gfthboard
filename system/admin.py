d265bd33b1d64343148b1e7d01923fb90d4ae6d7
gfthamnaj <60995426+gfthamnaj@users.noreply.github.com>
2020-03-12 16:23:40 +0700

    Dev (#21)
    * Add counting number
    
    * Fixed employee menu.
    
    * Add Shift Type column
    
    * Add start date.
    
    * Add initial files.
    
    * Add initial files.
    
    * Change app icon color.
    
    * Fix modal dialog height
    
    * Change label
    
    * Fix typo
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Fix query
    
    * Fix query
    
    * Fix db name
    
    * Add search box on Contract Policy admin page.
    
    Co-authored-by: Amnaj Potipak <apotipak@gmail.com>

--- system/admin.py
+++ system/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
-from system.models import ContractPolicy
+from .models import ContractPolicy
 
 
-admin.site.register(ContractPolicy)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created_on')
+    search_fields = ['name', 'email']
+    ordering = ['-name']
+    list_filter = ['active']
+    date_hierarchy = 'created_on'
+
+
+class ContractPolicyAdmin(admin.ModelAdmin):    
+	list_diaplay = ['username', 'zone_id', 'ComZone__zone_th']
+	search_fields = ['username__username', 'zone_id__zone_en']
+
+
+admin.site.register(ContractPolicy, ContractPolicyAdmin)
