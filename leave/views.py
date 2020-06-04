b831d758a48dbe78db5545e59de64d471533feaf
Amnaj Potipak <amnaj.potipak@guardforce.co.th>
2020-06-04 08:29:03 +0700

    Adjust leave history table

--- leave/views.py
+++ leave/views.py
@@ -35,7 +35,7 @@ class EmployeeInstanceListView(PermissionRequiredMixin, generic.ListView):
     project_version = settings.PROJECT_VERSION
     today_date = settings.TODAY_DATE    
     model = EmployeeInstance
-    paginate_by = 20
+    #paginate_by = 20
 
     def get_context_data(self, **kwargs):
         context = super(EmployeeInstanceListView, self).get_context_data(**kwargs)
