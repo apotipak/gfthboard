fc6d1dd297d9d22938c01c578bc6c56d4a2db550
gfthamnaj <60995426+gfthamnaj@users.noreply.github.com>
2020-06-18 15:29:02 +0700

    Fix missing hour (#101)
    * Revised content
    
    * Revised content
    
    * Revised content
    
    * Change link address
    
    * Fix receipient address
    
    * Fix typo
    
    * Fix start/end date hour range
    
    * Add link into no. of leave request
    
    * Revised content
    
    * Fix 8 hrs not show in day
    
    * Merge start date and end date in one column
    
    * Fix left menu can't be open
    
    * Merge start date and end date in one column
    
    * Fix left menu can't be open
    
    * Fix left menu can't be open
    
    * Fix left menu can't be open
    
    * Fix refused to execute script
    
    * Fix testing
    
    * Fix testing
    
    * Fix incorrect hour
    
    * Fix hour calculation
    
    * Fix javascript error
    
    * Fix javascript error
    
    * Fix javascript error
    
    * Fix javascript error
    
    * Add link on number of leave
    
    * Switch EmployeeForm to use EmployeeM1817Form
    
    * Merge start / end date into one column
    
    * Fix choose wrong form
    
    * Fix error label
    
    * Change error label
    
    * Revised
    
    * Add 503 file for maintenance mode
    
    * Add maintenance mode file
    
    * Fix ma page
    
    * Fix hour range
    
    * Add leave request date
    
    * Fix send mail when cancel leave
    
    * Fix all email layout
    
    * Fix hour calculation
    
    * Fix server name
    
    * Fix server name
    
    * Fix version name
    
    * Fix hour
    
    * Fix hour
    
    * Fix hour
    
    * Fix hour
    
    * Fix hour
    
    * Add number of leave day
    
    * Fix date time layout
    
    * Swap color
    
    * Fix m1247 form layout
    
    * Add notification feature
    
    * Revised content
    
    * Add notification
    
    * Change email
    
    * Fix hour
    
    * Fix hour
    
    * Fix hour
    
    * Add dummy data
    
    * Revised e-leave document content
    
    * Add email ref
    
    * Add download link
    
    * Add help hard copy file.
    
    * Revised content
    
    * Delete eleave_v_001_help.pdf
    
    * Add help file
    
    * Add initial file
    
    * Add initial file
    
    * Add initial file
    
    * Open document in new tab
    
    * Break line
    
    * Show cancel leave item
    
    * Migrate
    
    * Add number of waiting list
    
    * Change leave cancel icon
    
    * Remove space
    
    * Change rule to approve leave request
    
    * Change how to see My Team
    
    * Fix hour calculation
    
    * Fix hour calculation
    
    * Add new hour calculation for m1817
    
    * Add calculate button for m1817
    
    * Revised error message
    
    * Fix Loading.. status
    
    * Fix error message
    
    * Fix missing hour
    
    Co-authored-by: apotipak <apotipak@gmail.com>

--- leave/rules.py
+++ leave/rules.py
@@ -26,40 +26,41 @@ def checkM1817TotalHours(employee_type, d1, d2, leave_type_id):
 
 	total_hour = 0
 	total_minute = 0
-	grand_total_hour = 0
-	count = 0
+	grand_total_hour = 0.0
+	hour = 0
 	
-	while (end_date >= start_date):
-		if start_date.weekday() not in excluded_day:
-			if (start_date.day == end_date.day):
-				if (start_date.hour <= start_working_hour):
-					if(end_date.hour <= stop_working_hour):
-						for i in range(start_working_hour, end_date.hour):
-							if i not in excluded_hour:
-								total_hour += 1
-								total_minute += 60
-					else:
-						for i in range(start_working_hour, stop_working_hour):
-							if i not in excluded_hour:
-								total_hour += 1
-								total_minute += 60
-				else:
-					for i in range(start_date.hour, end_date.hour):
-						if i not in excluded_hour:
-							total_hour += 1
-							total_minute += 60
-			else:
-				for i in range(start_date.hour, stop_working_hour):
+	while d2 >= d1:
+		if d1.weekday() < d2.weekday():
+			print("todo1 : " + str(d1))
+			if d1.weekday() not in excluded_day:
+				for i in range(start_working_hour, stop_working_hour):
 					if i not in excluded_hour:
-						total_hour += 1
-						total_minute += 60
+						hour += 1
+		elif d1.weekday() == d2.weekday():
+			print("todo2 : " + str(d1) + " | " + str(d2))
+			if d1.weekday() not in excluded_day:
+				for i in range(d1.hour, d2.hour):
+					if i not in excluded_hour:
+						hour += 1
+		else:
+			print("error!")
 
-		grand_total_hour += total_hour
-		total_hour = 0
-		start_date += dayDelta
+		grand_total_hour += hour
+		d1 += dayDelta
+		hour = 0
 
-	count = checkM1247TotalHours('M1247', d1_temp, d2_temp, leave_type_id)	
-	if not count.is_integer():
+	# grand_total_hour = checkM1817BusinessRules('M1817', d1_temp, d2_temp, leave_type_id)
+
+	print("grand_total_hour : " + str(grand_total_hour))
+
+	total_minute = (end_date - start_date).total_seconds() / 60.0
+	if (total_minute % 60) > 0:
+		return True, _("ช่วงวันลามีเศษครึ่งชั่วโมง")
+
+	if grand_total_hour <= 0:
+		return True, _("เลือกวันลาไม่ถูกต้อง")
+
+	if not grand_total_hour.is_integer():
 		return True, _("ช่วงวันลามีเศษครึ่งชั่วโมง")
 
 	return False, grand_total_hour
@@ -116,7 +117,7 @@ def checkM1247StandardBusinessRules(start_date, end_date, leave_type_id):
 		print("debug 1 : " + str(total_hour))
 
 		if total_hour <= 0:
-			return True, _("วันลาตรงกับเสาร์-อาทิตย์")
+			return True, _("เลือกวันลาไม่ถูกต้อง")
 
 		if total_hour.is_integer():
 			return False, total_hour
@@ -189,51 +190,44 @@ def checkM1247BusinessRules(employee_type, start_date, end_date, leave_type_id):
 
 
 def checkM1817BusinessRules(employee_type, start_date, end_date, leave_type_id):
+	d1 = start_date
+	d2 = end_date
+
+	hour = 0
+	total_hour = 0
+
 	start_working_hour = 8
 	stop_working_hour = 17
-	
-	leave_type_include_weekend_list = {'6','7','10','11','13','15'}
+
+	leave_type_include_weekend_list = {'6', '7', '10', '11', '13', '15'}
 	if leave_type_id not in leave_type_include_weekend_list:
-		excluded_day = {5, 6}		
+		excluded_day = {5, 6}
 	else:
 		excluded_day = {}
 
 	excluded_hour = {0, 1, 2, 3, 4, 5, 6, 7, 12, 18, 19, 20, 21, 22, 23}
 
-	total_hour = 0
-	total_minute = 0
-	grand_total_hour = 0
-	
-	while (end_date >= start_date):
-		if start_date.weekday() not in excluded_day:
-			if (start_date.day == end_date.day):
-				if (start_date.hour <= start_working_hour):
-					if(end_date.hour <= stop_working_hour):
-						for i in range(start_working_hour, end_date.hour):
-							if i not in excluded_hour:
-								total_hour += 1
-								total_minute += 60
-					else:
-						for i in range(start_working_hour, stop_working_hour):
-							if i not in excluded_hour:
-								total_hour += 1
-								total_minute += 60
-				else:
-					for i in range(start_date.hour, end_date.hour):
-						if i not in excluded_hour:
-							total_hour += 1
-							total_minute += 60
-			else:
-				for i in range(start_date.hour, stop_working_hour):
+	while d2 >= d1:
+		if d1.weekday() < d2.weekday():
+			print("todo1 : " + str(d1))
+			if d1.weekday() not in excluded_day:
+				for i in range(start_working_hour, stop_working_hour):
 					if i not in excluded_hour:
-						total_hour += 1
-						total_minute += 60
+						hour += 1
+		elif d1.weekday() == d2.weekday():
+			print("todo2 : " + str(d1) + " | " + str(d2))
+			if d1.weekday() not in excluded_day:
+				for i in range(d1.hour, d2.hour):
+					if i not in excluded_hour:
+						hour += 1
+		else:
+			print("error!")
 
-		grand_total_hour += total_hour
-		total_hour = 0
-		start_date += dayDelta
+		total_hour += hour
+		d1 += dayDelta
+		hour = 0
 
-	return grand_total_hour
+	return total_hour
 
 
 def checkLeaveRequestApproval(username):
