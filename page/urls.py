4860a9bf68c2234f1d0d2125a864aec828b7fe3d
gfthamnaj <60995426+gfthamnaj@users.noreply.github.com>
2020-06-25 11:32:28 +0700

    Fix hour (#140)
    * Revised content
    
    * Revised content
    
    * Revised content
    
    * Change link address
    
    * Fix receipient address
    
    * Fix typo
    
    * Fix start/end date hour range
    
    * Add link into no. of leave request
    
    * Revised content
    
    * Fix 8 hrs not show in day
    
    * Merge start date and end date in one column
    
    * Fix left menu can't be open
    
    * Merge start date and end date in one column
    
    * Fix left menu can't be open
    
    * Fix left menu can't be open
    
    * Fix left menu can't be open
    
    * Fix refused to execute script
    
    * Fix testing
    
    * Fix testing
    
    * Fix incorrect hour
    
    * Fix hour calculation
    
    * Fix javascript error
    
    * Fix javascript error
    
    * Fix javascript error
    
    * Fix javascript error
    
    * Add link on number of leave
    
    * Switch EmployeeForm to use EmployeeM1817Form
    
    * Merge start / end date into one column
    
    * Fix choose wrong form
    
    * Fix error label
    
    * Change error label
    
    * Revised
    
    * Add 503 file for maintenance mode
    
    * Add maintenance mode file
    
    * Fix ma page
    
    * Fix hour range
    
    * Add leave request date
    
    * Fix send mail when cancel leave
    
    * Fix all email layout
    
    * Fix hour calculation
    
    * Fix server name
    
    * Fix server name
    
    * Fix version name
    
    * Fix hour
    
    * Fix hour
    
    * Fix hour
    
    * Fix hour
    
    * Fix hour
    
    * Add number of leave day
    
    * Fix date time layout
    
    * Swap color
    
    * Fix m1247 form layout
    
    * Add notification feature
    
    * Revised content
    
    * Add notification
    
    * Change email
    
    * Fix hour
    
    * Fix hour
    
    * Fix hour
    
    * Add dummy data
    
    * Revised e-leave document content
    
    * Add email ref
    
    * Add download link
    
    * Add help hard copy file.
    
    * Revised content
    
    * Delete eleave_v_001_help.pdf
    
    * Add help file
    
    * Add initial file
    
    * Add initial file
    
    * Add initial file
    
    * Open document in new tab
    
    * Break line
    
    * Show cancel leave item
    
    * Migrate
    
    * Add number of waiting list
    
    * Change leave cancel icon
    
    * Remove space
    
    * Change rule to approve leave request
    
    * Change how to see My Team
    
    * Fix hour calculation
    
    * Fix hour calculation
    
    * Add new hour calculation for m1817
    
    * Add calculate button for m1817
    
    * Revised error message
    
    * Fix Loading.. status
    
    * Fix error message
    
    * Fix missing hour
    
    * Revised UI
    
    * Fix wrong hour calculation
    
    * Fix wrong hour cal
    
    * Add timeline feature
    
    * Fix december color
    
    * Revised timeline layout
    
    * Hide title
    
    * Remove typo
    
    * Show not item displayed
    
    * Add begining timeline
    
    * Change color for Oct
    
    * Add more colors
    
    * Reviesed layout
    
    * Revised label
    
    * Fix hour calculation
    
    * Fix font size
    
    * Add new feature
    
    * Add initial files
    
    * test
    
    * Added
    
    * Add files
    
    * Add files
    
    * Test feature
    
    * hide on mobile
    
    * Fix file attached
    
    * Remove file
    
    * Fix attached file
    
    * Fix file size
    
    * Increase attached file size
    
    * fix empty attached file
    
    * Fix attached file
    
    * Disabl button while being processed
    
    * Fix attached file
    
    * Fix attached file
    
    * Fix attached file
    
    * Fix attached file.
    
    * Fix attached file
    
    * Add file
    
    * Add attached file icon
    
    * Add login background image
    
    * Decrease background image opacity
    
    * Change label
    
    * Fix date range
    
    * Revised menu color
    
    * Fix date range for all M1
    
    * Fix hour calculation
    
    * Fix menu bar space
    
    * Fix hour
    
    Co-authored-by: apotipak <apotipak@gmail.com>

--- page/urls.py
+++ page/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('staff-profile', views.StaffProfile, name='staff-profile'),
     path('staff-password', views.StaffPassword, name='staff-password'),
+    path('staff-language', views.StaffLanguage, name='staff-language'),
     path('help-eleave', views.HelpEleave, name='help-eleave'),
 ]
