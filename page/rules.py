c3522641a76ed78101e4b545f54ce16050d8e1be
Amnaj Potipak <amnaj.potipak@guardforce.co.th>
2020-06-25 16:00:30 +0700

    Fix object not existed

--- page/rules.py
+++ page/rules.py
@@ -1,11 +1,18 @@
 from django.contrib.auth.models import User
 from page.models import UserProfile
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def getDefaultLanguage(username):
-	default_language = UserProfile.objects.filter(username=username).values_list('language', flat=True).get()
-	if default_language:
-		return default_language
-	else:
-		return 'en'
+	default_language = 'en'
+	
+	if UserProfile.objects.filter(username=username).exists():
+		try:
+			default_language = UserProfile.objects.filter(username=username).values_list('language', flat=True).get()
+		except UserProfile.DoesNotExists:
+			default_language = 'en'
+	
+	print("debug: " + default_language)
+
+	return default_language
 
