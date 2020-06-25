08cc117fbbfdcea4cb3556bf6c2a848b279de2d8
Amnaj Potipak <amnaj.potipak@guardforce.co.th>
2020-06-25 14:50:53 +0700

    Add switch language

--- page/forms.py
+++ page/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from .models import UserProfile
 from django import forms
 import re
+from django.utils.translation import ugettext_lazy as _
 
 
 class UserForm(ModelForm):
@@ -92,9 +93,9 @@ class UserForm(ModelForm):
 	'''
 
 class LanguageForm(ModelForm):
-	language_options = (('en','en'),('th','th'))
-	language_code = forms.CharField(max_length=2, widget=forms.Select(choices=language_options), initial=0)	
-
+	language_options = (('en','English'),('th','Thai'))
+	language_code = forms.CharField(label=_('Set Display Language'), max_length=2, widget=forms.Select(choices=language_options), initial=0)	
+	
 	class Meta:
 		model = UserProfile
 		fields = ['language']
@@ -105,7 +106,6 @@ class LanguageForm(ModelForm):
 		self.fields['language_code'].widget.attrs={'class': 'form-control'}
         
 	def clean(self):
-		cleaned_data = super(UserForm, self).clean()
+		cleaned_data = super(LanguageForm, self).clean()
 		language = self.cleaned_data.get('language_code')
-
 		return cleaned_data
