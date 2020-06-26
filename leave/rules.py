df7b8a49d39a5d8b171e9a156495cf412ee4de73
Amnaj Potipak <amnaj.potipak@guardforce.co.th>
2020-06-26 14:09:27 +0700

    Fix hour calculation

--- leave/rules.py
+++ leave/rules.py
@@ -7,6 +7,180 @@ from leave.models import LeaveEmployee
 
 dayDelta = timedelta(days=1)
 
+
+def checkM1247TotalHours(employee_type, start_date, end_date, leave_type_id):
+	total_day = 0
+	total_hour = 0
+	result = 0
+	day = 0
+	hour = 0
+
+	leave_type_include_weekend_list = {'6', '7', '10', '11', '13', '15'}
+	if leave_type_id not in leave_type_include_weekend_list:
+		# excluded_day = {5, 6}
+		excluded_day = {}
+	else:
+		excluded_day = {}
+
+	while end_date >= start_date:
+		next_date = start_date + dayDelta
+
+		if next_date < end_date:
+			if start_date.weekday() not in excluded_day:
+				result = ((next_date - start_date).total_seconds() / 60.0) / 60
+				day = result // 24
+				hour = round(result % 24,1)
+
+			if hour > 8:
+				hour = 8
+			elif hour > 4:
+				hour = 4
+
+		else:
+			if start_date.weekday() not in excluded_day:
+				result = ((end_date - start_date).total_seconds() / 60.0) / 60
+				day = result // 24
+				hour = round(result % 24,1)
+
+			if hour > 8:
+				hour = 8
+			elif hour > 4:
+				hour = 4				
+
+		total_day = total_day + day
+		total_hour = total_hour + hour
+		day = 0
+		hour = 0
+		start_date += dayDelta
+
+	if total_hour == 24:
+		grand_total_hour = (total_day * 8) + (total_hour / 24) * 8
+	else:
+		grand_total_hour = (total_day * 8) + total_hour	
+	
+	# print("debug 1 : " + str(grand_total_hour))
+	print("debug 1 day : " + str(total_day))
+	print("debug 1 hour : " + str(total_hour))
+
+	return grand_total_hour
+
+def checkM1247BusinessRules(employee_type, start_date, end_date, leave_type_id):
+	total_day = 0
+	total_hour = 0
+	result = 0
+	day = 0
+	hour = 0
+
+	leave_type_include_weekend_list = {'6', '7', '10', '11', '13', '15'}
+	if leave_type_id not in leave_type_include_weekend_list:
+		#excluded_day = {5, 6}
+		excluded_day = {}
+	else:
+		excluded_day = {}
+
+	while end_date >= start_date:
+		next_date = start_date + dayDelta
+
+		if next_date < end_date:
+			if start_date.weekday() not in excluded_day:
+				result = ((next_date - start_date).total_seconds() / 60.0) / 60
+				day = result // 24
+				hour = round(result % 24,1)
+
+			if hour > 8:
+				hour = 8
+			elif hour > 4:
+				hour = 4
+
+		else:
+			if start_date.weekday() not in excluded_day:
+				result = ((end_date - start_date).total_seconds() / 60.0) / 60
+				day = result // 24
+				hour = round(result % 24,1)
+
+			if hour > 8:
+				hour = 8
+			elif hour > 4:
+				hour = 4				
+
+		total_day = total_day + day
+		total_hour = total_hour + hour
+		day = 0
+		hour = 0
+		start_date += dayDelta
+
+	if total_hour == 24:
+		grand_total_hour = (total_day * 8) + (total_hour / 24) * 8
+	else:
+		grand_total_hour = (total_day * 8) + total_hour	
+
+	return False, grand_total_hour
+
+
+def checkM1247TotalHours1(employee_type, d1, d2, leave_type_id):
+    d1_temp = d1
+    d2_temp = d2
+    start_date = d1
+    end_date = d2
+
+    start_working_hour = 0
+    stop_working_hour = 23
+
+    leave_type_include_weekend_list = {'6', '7', '10', '11', '13', '15'}
+    if leave_type_id not in leave_type_include_weekend_list:
+        #excluded_day = {5, 6}
+        excluded_day = {}
+    else:
+        excluded_day = {}
+
+    # excluded_hour = {0, 1, 2, 3, 4, 5, 6, 7, 12, 18, 19, 20, 21, 22, 23}
+    excluded_hour = {}
+
+    total_hour = 0
+    total_minute = 0
+    grand_total_hour = 0.0
+    hour = 0
+
+    while d2 >= d1:
+        if d1.weekday() < d2.weekday():
+            print("todo1 : " + str(d1))
+            if d1.weekday() not in excluded_day:
+                for i in range(start_working_hour, stop_working_hour):
+                    if i not in excluded_hour:
+                        hour += 1
+
+            if hour > 8:
+            	hour = 8            	
+        elif d1.weekday() == d2.weekday():
+            print("todo2 : " + str(d1) + " | " + str(d2))
+            if d1.weekday() not in excluded_day:
+                for i in range(d1.hour, d2.hour):
+                    if i not in excluded_hour:
+                        hour += 1
+            if hour > 8:
+            	hour = 8
+        else:
+            print("todo3 : " + str(d1) + " | " + str(d2))
+            if d1.weekday() not in excluded_day:
+                for i in range(start_working_hour, stop_working_hour):
+                    if i not in excluded_hour:
+                        hour += 1
+
+            if hour > 8:
+            	hour = 8
+
+        grand_total_hour += hour
+        d1 += dayDelta
+        hour = 0
+
+    if total_hour == 24:
+    	grand_total_hour = (grand_total_hour / 24) * 8
+
+    print("debug 2: " + str(grand_total_hour))
+
+    return grand_total_hour
+
+
 def checkAllEmployeeType(employee_type, d1, d2, leave_type_id):
     d1_temp = d1
     d2_temp = d2
@@ -129,170 +303,6 @@ def checkM1817TotalHours(employee_type, d1, d2, leave_type_id):
 
 	if not grand_total_hour.is_integer():
 		return True, _("ช่วงวันลามีเศษครึ่งชั่วโมง")
-
-	return False, grand_total_hour
-
-
-def checkM1247TotalHours1(employee_type, d1, d2, leave_type_id):
-    d1_temp = d1
-    d2_temp = d2
-    start_date = d1
-    end_date = d2
-
-    start_working_hour = 0
-    stop_working_hour = 23
-
-    leave_type_include_weekend_list = {'6', '7', '10', '11', '13', '15'}
-    if leave_type_id not in leave_type_include_weekend_list:
-        #excluded_day = {5, 6}
-        excluded_day = {}
-    else:
-        excluded_day = {}
-
-    # excluded_hour = {0, 1, 2, 3, 4, 5, 6, 7, 12, 18, 19, 20, 21, 22, 23}
-    excluded_hour = {}
-
-    total_hour = 0
-    total_minute = 0
-    grand_total_hour = 0.0
-    hour = 0
-
-    while d2 >= d1:
-        if d1.weekday() < d2.weekday():
-            print("todo1 : " + str(d1))
-            if d1.weekday() not in excluded_day:
-                for i in range(start_working_hour, stop_working_hour):
-                    if i not in excluded_hour:
-                        hour += 1
-
-            if hour > 8:
-            	hour = 8            	
-        elif d1.weekday() == d2.weekday():
-            print("todo2 : " + str(d1) + " | " + str(d2))
-            if d1.weekday() not in excluded_day:
-                for i in range(d1.hour, d2.hour):
-                    if i not in excluded_hour:
-                        hour += 1
-            if hour > 8:
-            	hour = 8
-        else:
-            print("todo3 : " + str(d1) + " | " + str(d2))
-            if d1.weekday() not in excluded_day:
-                for i in range(start_working_hour, stop_working_hour):
-                    if i not in excluded_hour:
-                        hour += 1
-
-            if hour > 8:
-            	hour = 8
-
-        grand_total_hour += hour
-        d1 += dayDelta
-        hour = 0
-
-    if total_hour == 24:
-    	grand_total_hour = (grand_total_hour / 24) * 8
-
-    print("debug 2: " + str(grand_total_hour))
-
-    return grand_total_hour
-
-
-def checkM1247TotalHours(employee_type, start_date, end_date, leave_type_id):
-	total_day = 0
-	total_hour = 0
-	result = 0
-	day = 0
-	hour = 0
-
-	leave_type_include_weekend_list = {'6', '7', '10', '11', '13', '15'}
-	if leave_type_id not in leave_type_include_weekend_list:
-		# excluded_day = {5, 6}
-		excluded_day = {}
-	else:
-		excluded_day = {}
-
-	while end_date >= start_date:
-		next_date = start_date + dayDelta
-
-		if next_date < end_date:
-			if start_date.weekday() not in excluded_day:
-				result = ((next_date - start_date).total_seconds() / 60.0) / 60
-				day = result // 24
-				hour = round(result % 24,1)
-
-			if hour > 8:
-				hour = 8
-		else:
-			if start_date.weekday() not in excluded_day:
-				result = ((end_date - start_date).total_seconds() / 60.0) / 60
-				day = result // 24
-				hour = round(result % 24,1)
-
-			if hour > 8:
-				hour = 8
-
-		total_day = total_day + day
-		total_hour = total_hour + hour
-		day = 0
-		hour = 0
-		start_date += dayDelta
-
-	if total_hour == 24:
-		grand_total_hour = (total_day * 8) + (total_hour / 24) * 8
-	else:
-		grand_total_hour = (total_day * 8) + total_hour	
-	
-	# print("debug 1 : " + str(grand_total_hour))
-	print("debug 1 day : " + str(total_day))
-	print("debug 1 hour : " + str(total_hour))
-
-	return grand_total_hour
-
-
-def checkM1247BusinessRules(employee_type, start_date, end_date, leave_type_id):
-	total_day = 0
-	total_hour = 0
-	result = 0
-	day = 0
-	hour = 0
-
-	leave_type_include_weekend_list = {'6', '7', '10', '11', '13', '15'}
-	if leave_type_id not in leave_type_include_weekend_list:
-		#excluded_day = {5, 6}
-		excluded_day = {}
-	else:
-		excluded_day = {}
-
-	while end_date >= start_date:
-		next_date = start_date + dayDelta
-
-		if next_date < end_date:
-			if start_date.weekday() not in excluded_day:
-				result = ((next_date - start_date).total_seconds() / 60.0) / 60
-				day = result // 24
-				hour = round(result % 24,1)
-
-			if hour > 8:
-				hour = 8
-		else:
-			if start_date.weekday() not in excluded_day:
-				result = ((end_date - start_date).total_seconds() / 60.0) / 60
-				day = result // 24
-				hour = round(result % 24,1)
-
-			if hour > 8:
-				hour = 8
-
-		total_day = total_day + day
-		total_hour = total_hour + hour
-		day = 0
-		hour = 0
-		start_date += dayDelta
-
-	if total_hour == 24:
-		grand_total_hour = (total_day * 8) + (total_hour / 24) * 8
-	else:
-		grand_total_hour = (total_day * 8) + total_hour	
 
 	return False, grand_total_hour
 
