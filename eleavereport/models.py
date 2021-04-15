3dcdacdf8c4fd98275244dd2d5feb87e92928b9a
Amnaj Potipak <amnaj.potipak@guardforce.co.th>
2021-04-15 17:53:03 +0700

    Add initial files.

--- eleavereport/models.py
+++ eleavereport/models.py
@@ -5,6 +5,5 @@ class EleaveReports(models.Model):
     class Meta:
         permissions = (
             ("can_view_m1_report", "Can view M1 report"),
-            # ("can_view_m1_pending_leave_request_report", "Can view M1 pending leave request report"),
-            ("can_view_m1_approved_leave_request_report", "Can view M1 approved leave request report"),
+            ("can_view_m1_leave_report", "Can view M1 leave report"),
         )
--- eleavereport/urls.py
+++ eleavereport/urls.py
@@ -7,6 +7,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.ViewM1Report, name='view_m1_report'),
-    # path('view-m1-pending-leave-request-report', views.ViewM1PendingLeaveRequestReport, name='view_m1_pending_leave_request_report'),
-    path('view-m1-approved-leave-request-report', views.ViewM1ApprovedLeaveRequestReport, name='view_m1_approved_leave_request_report'),
+    path('view-m1-leave-report', views.ViewM1LeaveReport, name='view_m1_leave_report'),
 ]
