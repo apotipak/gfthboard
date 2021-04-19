05efdebd7a5c2f9721113a2c1323bae4708efa25
Amnaj Potipak <amnaj.potipak@guardforce.co.th>
2021-04-19 10:40:22 +0700

    Add initial files.

---
+++ eleaveadmin/forms.py
@@ -0,0 +1,192 @@
+import datetime
+import time
+import calendar
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
+from datetime import datetime, timedelta
+from django import forms
+from leave.models import LeavePlan, LeaveHoliday, LeaveEmployee, LeaveType, EmployeeInstance
+from django.forms.widgets import HiddenInput
+from django.contrib.auth.models import User
+from decimal import Decimal
+from django.db.models import Sum
+from .rules import *
+from django.utils.dateparse import parse_datetime
+from django import forms
+from django.template.defaultfilters import filesizeformat
+from django.forms import Textarea
+
+
+current_year = datetime.now().year
+standard_start_working_hour = 0
+standard_stop_working_hour = 23
+
+class EmployeeM1247Form(forms.ModelForm):
+    start_working_hour = 0
+    stop_working_hour = 59
+    hour_range = ((0,'00'),(1,'01'),(2,'02'),(3,'03'),(4,'04'),(5,'05'),(6,'06'),(7,'07'),(8,'08'),(9,'09'),(10,'10'),(11,'11'),(12,'12'),(13,'13'),(14,'14'),(15,'15'),(16,'16'),(17,'17'),(18,'18'),(19,'19'),(20,'20'),(21,'21'),(22,'22'),(23,'23'))
+    minute_range = ((0,'00'),(30,'30'),(59,'59'))
+
+    leave_type = forms.ModelChoiceField(label=_('Select Leave Type'), queryset=None, required=True)
+
+    start_date = forms.DateField(label=_('Start'), required=True, error_messages={'required': 'กรุณาป้อนข้อมูลวันที่ลา'})
+    start_hour = forms.IntegerField(widget=forms.Select(choices=hour_range))
+    start_minute = forms.IntegerField(widget=forms.Select(choices=minute_range))
+
+    end_date = forms.DateField(label=_('End'), required=True, error_messages={'required': 'กรุณาป้อนข้อมูลลาถึงวันที่'})    
+    end_hour = forms.IntegerField(widget=forms.Select(choices=hour_range))
+    end_minute = forms.IntegerField(widget=forms.Select(choices=minute_range))
+
+    employee_type = forms.CharField(required=False, widget=forms.HiddenInput(), initial="M1247")
+
+    document = forms.FileField(required=False)
+
+    leave_reason = forms.CharField(required=False, widget=forms.Textarea(attrs={'rows': 2, 'cols': 20}), max_length=200)
+
+    class Meta:
+        model = EmployeeInstance
+        fields = ['start_date', 'end_date', 'leave_type', 'lve_act', 'lve_act_hr', 'document', 'leave_reason']
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user')
+        super(EmployeeM1247Form, self).__init__(*args, **kwargs)
+
+        default_end_date =  datetime.now()+timedelta(days=1)
+        self.fields['leave_type'].widget.attrs={'class': 'form-control'}
+        self.fields['leave_type'].queryset=LeaveType.objects.filter(leaveplan__emp_id=self.user.username, leaveplan__lve_year=current_year)        
+
+        self.fields['start_date'].widget.attrs={'class': 'form-control datepicker border-bottom-1 border-left-0 rounded-0 bg-white'}
+        self.initial['start_date'] = datetime.now().strftime("%Y-%m-%d")
+        self.fields['start_date'].widget.attrs['placeholder'] = "YYYY-MM-DD"        
+        self.fields['start_hour'].widget.attrs={'class': 'form-control border-top-0 border-left-1 rounded-0 bg-white'}
+        self.initial['start_hour'] = 8
+        self.fields['start_minute'].widget.attrs={'class': 'form-control border-top-0 rounded-0 bg-white'}
+        self.initial['start_minute'] = 0
+
+        self.fields['end_date'].widget.attrs={'class': 'form-control datepicker border-bottom-1 border-left-0 rounded-0 bg-white'}
+        self.initial['end_date'] = datetime.now().strftime("%Y-%m-%d") #default_end_date.strftime("%Y-%m-%d")
+        self.fields['end_date'].widget.attrs['placeholder'] = "YYYY-MM-DD"        
+        self.fields['end_hour'].widget.attrs={'class': 'form-control border-top-0 border-left-1 rounded-0 bg-white'}
+        self.initial['end_hour'] = 17
+        self.fields['end_minute'].widget.attrs={'class': 'form-control border-top-0 rounded-0 bg-white'}
+        self.initial['end_minute'] = 0
+
+        self.fields['leave_reason'].widget.attrs={'class': 'form-control rounded-0'}
+
+    def clean(self):
+        datetime_format = "%Y-%m-%d %H:%M:%S"
+        cleaned_data = super(EmployeeM1247Form, self).clean()
+
+        leave_type = self.cleaned_data.get('leave_type')
+        leave_type_id = self.data.get('leave_type')
+
+        start_date = self.cleaned_data.get('start_date')
+        start_hour = self.cleaned_data.get('start_hour')
+        start_minute = self.cleaned_data.get('start_minute')
+
+        end_date = self.cleaned_data.get('end_date')
+        end_hour = self.cleaned_data.get('end_hour')
+        end_minute = self.cleaned_data.get('end_minute')
+        
+        document = self.cleaned_data.get('document')
+        if document is not None:
+            document_type = document.content_type.split('/')[1]
+            print("type:",  document_type)
+            if document_type in settings.CONTENT_TYPES:
+                if document.size > settings.MAX_UPLOAD_SIZE:
+                    raise forms.ValidationError(_('ไฟล์แนบมีขนาดเกิน 5 เมกะไบท์'))
+            else:
+                raise forms.ValidationError(_('ระบบสามารถแนบไฟล์ (png, jpg, jpeg, pdf) ได้เท่านั้น'))
+
+        # raise forms.ValidationError(_('Test'))
+
+        username = self.user.username
+        employee_type = 'M1'
+
+        d1 = str(start_date) + ' ' + str(start_hour) + ':' + str(start_minute) + ':00'
+        d2 = str(end_date) + ' ' + str(end_hour) + ':' + str(end_minute) + ':00'
+        start_date = datetime.strptime(d1, datetime_format)
+        end_date = datetime.strptime(d2, datetime_format)
+
+        if start_date != None:
+            # ------------------------------------------------ 
+            # Check master remaining hour (Leave_Plan table)
+            # ------------------------------------------------
+            leave_plan = LeavePlan.objects.filter(emp_id__exact=username).filter(lve_id__exact=leave_type_id).filter(lve_year=current_year).values_list('lve_plan', flat=True).get()
+            print("Plan : " + str(leave_plan))
+            total_leave_quota_remaining_day = LeavePlan.objects.filter(emp_id__exact=username).filter(lve_id__exact=leave_type_id).filter(lve_year=current_year).values_list('lve_miss', flat=True).get()
+            print("remain day : " + str(total_leave_quota_remaining_day))
+            total_leave_quota_remaining_hour = LeavePlan.objects.filter(emp_id__exact=username).filter(lve_id__exact=leave_type_id).filter(lve_year=current_year).values_list('lve_miss_hr', flat=True).get()                        
+            print("remain hour : " + str(total_leave_quota_remaining_hour))
+            grand_total_leave_quota_remaining_hour = (total_leave_quota_remaining_hour + (total_leave_quota_remaining_day * 8))
+            # 26d + 0h
+            # 26 * 8 = 208
+            print("total remain hour : " + str(grand_total_leave_quota_remaining_hour))
+
+            # ------------------------------------------------ 
+            # Check transaction remaing hour (leave_employeeinstance table) (filter status = p, a, F)
+            # ------------------------------------------------ 
+            total_pending_approve_syncfail_status_history_day = EmployeeInstance.objects.filter(emp_id__exact=username).filter(leave_type_id__exact=leave_type_id).filter(status__in=('p')).aggregate(sum=Sum('lve_act'))['sum'] or 0
+            print("sync day : " + str(total_pending_approve_syncfail_status_history_day))
+            total_pending_approve_syncfail_status_history_hour = EmployeeInstance.objects.filter(emp_id__exact=username).filter(leave_type_id__exact=leave_type_id).filter(status__in=('p')).aggregate(sum=Sum('lve_act_hr'))['sum'] or 0
+            print("sync hour : " + str(total_pending_approve_syncfail_status_history_hour))
+            grand_total_pending_approve_syncfail_status_history_hour = total_pending_approve_syncfail_status_history_hour + (total_pending_approve_syncfail_status_history_day * 8)
+            # 25d + 16h
+            # (25 * 8) + 16 = 216
+            print("total sync hour : " + str(grand_total_pending_approve_syncfail_status_history_hour))
+
+            grand_total_leave_quota_remaining_hour = grand_total_leave_quota_remaining_hour - grand_total_pending_approve_syncfail_status_history_hour
+            print("grand : " + str(grand_total_leave_quota_remaining_hour))
+
+            # ------------------------------------------------
+            # Check Standard Business Rules
+            # ------------------------------------------------
+            found_standard_error = checkM1247StandardBusinessRules(start_date, end_date, leave_type_id)
+            if found_standard_error[0]:
+                raise forms.ValidationError(_(found_standard_error[1]))
+            
+            # ------------------------------------------------ 
+            # Check M1247 Business Rules
+            # ------------------------------------------------            
+            found_m1247_error = checkM1247BusinessRules('M1247', start_date, end_date, leave_type_id)
+            if found_m1247_error[0]:
+                raise forms.ValidationError(found_m1247_error[1])
+            else:
+                total_leave_request_hour = found_m1247_error[1]
+
+            
+            #raise forms.ValidationError(total_leave_request_hour)
+            print(str(total_leave_request_hour) + " : total_leave_request_hour")
+            print(str(grand_total_leave_quota_remaining_hour) + " : 1grand_total_leave_quota_remaining_hour")
+
+            # RULE 1: Check not over leave quota
+            if (total_leave_request_hour) > grand_total_leave_quota_remaining_hour:
+                #raise forms.ValidationError({'start_date': "เลือกวันลาเกินโควต้าที่กำหนด"})
+                raise forms.ValidationError(_("เลือกวันเกินจำนวนวันลาที่กำหนด"))
+            else:
+                if grand_total_leave_quota_remaining_hour <= 0:
+                    #raise forms.ValidationError({'start_date': "ใช้วัน" + str(leave_type) + "หมดแล้ว" })
+                    raise forms.ValidationError(_("ใช้วัน" + str(leave_type) + "หมดแล้ว"))
+
+            # RULE 2: Check duplicate leave
+            #select id from leave_employeeinstance where not (start_date > @end_date OR end_date < @start_date)
+            queryset = EmployeeInstance.objects.raw("select id from leave_employeeinstance where not (start_date > '" + str(end_date.strftime("%Y-%m-%d %H:00") + "' or end_date < '" + str(start_date.strftime("%Y-%m-%d %H:01")) + "')") + " and emp_id=" + username + " and status in ('a','p','C','F')")            
+            if len(queryset) > 0:
+                #raise forms.ValidationError({'start_date': "เลือกวันลาซ้ำ"})
+                raise forms.ValidationError(_("มีการเลือกวันลาซ้ำ"))
+
+            # RULE 3: Check public holidays
+            if checkLeaveTypeIncludePublicHoliday(leave_type_id):
+                queryset = LeaveHoliday.objects.filter(hol_date__range=(start_date.strftime("%Y-%m-%d"), end_date.strftime("%Y-%m-%d"))).values_list('pub_th', flat=True)
+                holiday_list = str(list(queryset)).replace("'", '')
+                if len(queryset) > 0:
+                    #raise forms.ValidationError({'start_date': "เลือกวันลาตรงกับวันหยุด - " + str(holiday_list)})
+                    raise forms.ValidationError(_("ช่วงวันลาตรงกับวันหยุดนักขัตฤกษ์ - " + str(holiday_list)))
+
+            # RULE 4: Check not allow over month
+            if(checkLeaveRequestOverMonth("M1", start_date, end_date)):
+                #raise forms.ValidationError({'start_date': "เลือกวันลาข้ามเดือน"})
+                raise forms.ValidationError(_("ไม่สามารถเลือกวันลาข้ามเดือน กรุณาแบ่งทำ 2 รายการแยกเดือนกัน"))
+        
+        return cleaned_data    
--- eleaveadmin/models.py
+++ eleaveadmin/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from django.core.exceptions import ObjectDoesNotExist
+import datetime
+from django.utils.timezone import now
+from django import forms
+from django.urls import reverse
+import uuid
+
 
 class EleaveAdmin(models.Model):
     class Meta:
@@ -7,3 +17,186 @@ class EleaveAdmin(models.Model):
             ("can_create_m1_leave_request", "Can create M1 leave request"),
             ("can_create_m5_leave_request", "Can create M5 leave request"),
         )
+
+
+'''
+class LeaveHoliday(models.Model):
+    hol_date = models.DateTimeField()
+    pub_id = models.SmallIntegerField(blank=True, null=True)
+    pub_th = models.CharField(db_column='PUB_TH', max_length=50, blank=True, null=True)  # Field name made lowercase.
+    pub_en = models.CharField(db_column='PUB_EN', max_length=50, blank=True, null=True)  # Field name made lowercase.
+    upd_date = models.DateTimeField()
+    upd_by = models.CharField(max_length=6)
+
+    class Meta:
+        managed = False
+        db_table = 'LEAVE_HOLIDAY'
+
+    def PublicHolidayInstance():
+        try: 
+            public_holiday_instance = LeaveHoliday.objects.raw("select pub_id as id, pub_th, pub_en, hol_date from LEAVE_HOLIDAY order by hol_date")
+        except ObjectDoesNotExist:
+            public_holiday_instance = None   
+    
+        return public_holiday_instance
+
+
+class LeaveEmployee(models.Model):
+    emp_id = models.DecimalField(max_digits=7, decimal_places=0, db_column='emp_id', primary_key=True)
+    emp_fname_en = models.CharField(max_length=30, blank=True, null=True)
+    emp_lname_en = models.CharField(max_length=40, blank=True, null=True)
+    emp_fname_th = models.CharField(max_length=30, blank=True, null=True)
+    emp_lname_th = models.CharField(max_length=40, blank=True, null=True)
+    pos_th = models.CharField(max_length=50, blank=True, null=True)
+    pos_en = models.CharField(max_length=50, blank=True, null=True)
+    div_th = models.CharField(max_length=50, blank=True, null=True)
+    div_en = models.CharField(max_length=50, blank=True, null=True)
+    emp_type = models.CharField(max_length=2, blank=True, null=True)
+    emp_join_date = models.DateTimeField(blank=True, null=True)
+    emp_term_date = models.DateTimeField(blank=True, null=True)
+    upd_date = models.DateTimeField()
+    upd_by = models.CharField(max_length=6)
+    emp_passcode = models.CharField(db_column='Emp_passcode', max_length=4, blank=True, null=True)  # Field name made lowercase.
+    emp_spid = models.CharField(db_column='emp_SpID', max_length=10, blank=True, null=True)  # Field name made lowercase.
+    password = models.CharField(max_length=13)
+    
+    class Meta:
+        managed = False
+        db_table = 'Leave_employee'
+        ordering = ['emp_id']
+        permissions = (
+            ("approve_leaveplan", "Can approve leave employee"),
+        )
+
+    def EmployeeInstance(request):
+        EmployeeInstance = None
+        if request.user.username != 'superadmin':
+            employee_id = request.user.username
+            try:
+                EmployeeInstance = LeaveEmployee.objects.raw("select * from Leave_employee where emp_id='" + employee_id + "'")
+            except ObjectDoesNotExist:
+                EmployeeInstance = None
+
+        return EmployeeInstance
+
+    def SuperVisorInstance(request):
+        SuperVisorInstance = None
+        if request.user.username != 'superadmin':
+            EmployeeSupervisorID = LeaveEmployee.objects.filter(emp_id__exact=request.user.username).values_list('emp_spid', flat=True).get()
+            try:
+                SuperVisorInstance = LeaveEmployee.objects.raw("select * from Leave_employee where emp_id='" + str(EmployeeSupervisorID) + "'")
+            except ObjectDoesNotExist:
+                SuperVisorInstance = None
+
+        return SuperVisorInstance
+
+    def TeamMemberList(request):
+        TeamMemberList = None
+        EmployeeID = request.user.username
+        try:
+            TeamMemberList = LeaveEmployee.objects.raw("select * from Leave_employee where emp_spid='" + EmployeeID + "' order by emp_id")
+        except ObjectDoesNotExist:
+            TeamMemberList = None
+
+        return TeamMemberList
+
+
+class LeaveType(models.Model):
+    lve_id = models.DecimalField(primary_key=True, max_digits=3, decimal_places=0)
+    lve_th = models.CharField(max_length=50, blank=True, null=True)
+    lve_en = models.CharField(max_length=50, blank=True, null=True)
+    lve_code = models.CharField(max_length=3, blank=True, null=True)
+    pay_type = models.CharField(max_length=3, blank=True, null=True)
+    upd_date = models.DateTimeField(blank=True, null=True)
+    upd_by = models.CharField(max_length=10, blank=True, null=True)
+    upd_flag = models.CharField(max_length=1, blank=True, null=True)
+    lve_type = models.CharField(max_length=50, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'LEAVE_TYPE'
+
+    def __str__(self):
+        return '{0}'.format(self.lve_th)
+
+class LeavePlan(models.Model):
+    lve_year = models.DecimalField(max_digits=4, decimal_places=0, blank=True, null=True)
+    emp_id = models.DecimalField(max_digits=7, decimal_places=0, blank=True, null=True)
+    lve_id = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    lve_code = models.CharField(max_length=3, blank=True, null=True)
+    lve_plan = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    lve_plan_hr = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True)
+    lve_act = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    lve_act_hr = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True)
+    lve_miss = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    lve_miss_hr = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True)
+    upd_date = models.DateTimeField()
+    upd_by = models.CharField(max_length=6)
+    lve_id = models.ForeignKey(LeaveType, db_column="lve_id", on_delete=models.SET_NULL, null=True)
+    lve_HRMS = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+    lve_HRMS_HR = models.DecimalField(max_digits=3, decimal_places=0, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'Leave_Plan'
+
+    def EmployeeLeavePolicy(request):
+        UserName = request.user.username
+        now = datetime.datetime.now()
+        LeaveYear = str(now.year)
+
+        if UserName == 'superadmin':
+            EmployeeLeavePolicyInstance = ""
+        else:
+            EmployeeLeavePolicyInstance = LeavePlan.objects.raw("select lp.emp_id as id, lp.lve_year, lt.lve_id as lve_type_id, lp.lve_code, lp.lve_plan, lt.lve_th, lt.lve_en, lp.lve_act, lp.lve_act_hr, lp.lve_miss, lp.lve_miss_hr, lp.lve_HRMS, lp.lve_HRMS_HR from leave_plan lp inner join leave_type lt on lp.lve_id=lt.lve_id where lp.emp_id=" + UserName + " and lp.lve_year=" + LeaveYear)
+        return EmployeeLeavePolicyInstance
+
+
+class EmployeeInstance(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)   
+    emp = models.ForeignKey(LeaveEmployee, on_delete=models.SET_NULL, null=True)
+    start_date = models.DateTimeField(null=True, blank=True)
+    end_date = models.DateTimeField(null=True, blank=True)
+    created_date = models.DateTimeField(default=now, editable=False)
+    created_by = models.DecimalField(max_digits=7, decimal_places=0, null=True)
+    leave_type = models.DecimalField(max_digits=3, decimal_places=0)
+    leave_type = models.ForeignKey(LeaveType, on_delete=models.SET_NULL, null=True, blank=True)    
+    lve_act = models.IntegerField(blank=True, null=True)
+    lve_act_hr = models.IntegerField(blank=True, null=True)
+    updated_date = models.DateTimeField(null=True, blank=True)
+    updated_by = models.DecimalField(max_digits=7, decimal_places=0, blank=True, null=True)    
+    comment = models.TextField(blank=True, null=True)
+    document = models.FileField(upload_to='documents/', null=True)
+    document_data = models.BinaryField(null=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True, null=True)
+    leave_reason = models.TextField(blank=True, null=True, max_length=200)
+    LEAVE_STATUS = (
+        ('a', 'Approved'),
+        ('p', 'Pending'),
+        ('r', 'Rejected'),
+        ('d', 'Deleted'),
+        ('c', 'Sync complete'),
+        ('f', 'Sync fail'),
+    )
+    status = models.CharField(
+        max_length=1,
+        choices=LEAVE_STATUS,
+        blank=True,
+        default='p',
+        help_text='Leave Request Status')
+    
+    class Meta:
+        #ordering = ['-created_date', '-start_date']
+        ordering = ['-created_date']
+
+    def get_absolute_url(self):
+        return reverse('leave_history_detail', args=[str(self.id)])
+
+    def TeamLeaveRequestCount(request):
+        queryset = EmployeeInstance.objects.raw("select id from leave_employeeinstance where emp_id in (select emp_id from leave_employee where emp_spid='" + request.user.username + "') and status='p'")
+        return len(queryset)
+
+    def __str__(self):
+        return '{0} ({1} {2})'.format(self.id, self.employee.emp_fname_th, self.status)
+'''
+
