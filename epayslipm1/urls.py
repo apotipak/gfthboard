47ec4e1955448d39ba04ef5dfe81273c030fa48c
Amnaj Potipak <amnaj.potipak@guardforce.co.th>
2021-04-22 16:21:11 +0700

    Add initial files.

--- epayslipm1/urls.py
+++ epayslipm1/urls.py
@@ -7,5 +7,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.EPaySlipM1, name='e_payslip_m1'),
-    url(r'^ajax/send_pay_slip_m1/$', views.AjaxSendPayslipM1, name='ajax_send_pay_slip_m1'),    
+    url(r'^ajax/send_pay_slip_m1/$', views.AjaxSendPayslipM1, name='ajax_send_pay_slip_m1'),
+    # url(r'^ajax/generate_payslip_m1/(?P<emp_id>\d+)/(?P<period>\w+)/$', views.generate_payslip_m1, name='generate_payslip_m1'),
 ]
