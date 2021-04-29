c06808f362669511e292713701844973a168faee
Amnaj Potipak <amnaj.potipak@guardforce.co.th>
2021-04-29 13:30:28 +0700

    Add initial files.

--- eleavereport/models.py
+++ eleavereport/models.py
@@ -4,6 +4,10 @@ from django.db import models
 class EleaveReports(models.Model):
     class Meta:
         permissions = (
-            ("can_view_m1_report", "Can view M1 report"),
+            ("can_view_m1_report", "Can view M1 reports"),
             ("can_view_m1_leave_report", "Can view M1 leave report"),
+            ("can_view_m3_report", "Can view M3 reports"),
+            ("can_view_m3_leave_report", "Can view M3 leave report"),            
+            ("can_view_m5_report", "Can view M5 reports"),
+            ("can_view_m5_leave_report", "Can view M5 leave report"),
         )
