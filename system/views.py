28f42ef99e27b7d2dfb5b3edce7c7465a3052f96
Amnaj Potipak <amnaj.potipak@guardforce.co.th>
2021-05-02 01:05:25 +0700

    Add initial files.

--- system/views.py
+++ system/views.py
@@ -15,11 +15,11 @@ from datetime import datetime
 from django.http import JsonResponse
 from django.utils import translation
 from django.utils.translation import ugettext as _
-# from numpy import genfromtxt
 from decimal import Decimal
 from os import path
 import django.db as db
 import csv
+import subprocess, sys
 
 
 class ContractPolicyListView(PermissionRequiredMixin, generic.ListView):
@@ -91,6 +91,28 @@ def ManageOutlookEmailActiveUserList(request):
 	        username_display = LeaveEmployee.objects.filter(emp_id=request.user.username).values_list('emp_fname_en', flat=True).get()
 	# End
 
+	# Get latest updated date
+	latest_updated = OutlookEmailActiveUserList.objects.values_list('created_date', flat=True).first()
+
+	office_365_email_list = []
+	record = {}
+
+	# Get current email list
+	outlook_email = OutlookEmailActiveUserList.objects.all().order_by('first_name')
+	if outlook_email is None:
+		office_365_email_list = None
+	else:
+		count = 0
+		for item in outlook_email:
+			record = {
+				"first_name": item.first_name,
+				"last_name": item.last_name,
+				"email": item.email,
+			}
+
+			office_365_email_list.append(record)
+			count = count + 1
+
 	return render(request, render_template_name, {
 		'page_title': settings.PROJECT_NAME,
 		'today_date': today_date,
@@ -101,107 +123,103 @@ def ManageOutlookEmailActiveUserList(request):
 		'username_display': username_display,
 		'available_period_obj': available_period_obj,
 		'available_period_list': list(available_period_list),
+		'last_updated': latest_updated.strftime("%d/%m/%Y %H:%M:%S"),
+		'office_365_email_list': office_365_email_list,
+		'total': count,
 	})
 
 
-
 @permission_required('system.add_outlookemailactiveuserlist', login_url='/accounts/login/')
-def ImportOutlokActiveUser(request):	
-	
-	csv_file = path.abspath("media\\system\\outlook.csv")
+def ImportOutlokActiveUser(request):
+	# Get latest updated date
+	latest_updated = OutlookEmailActiveUserList.objects.values_list('created_date', flat=True).first()
+	total = 0
+	count = 0	
+	office_365_email_list = []
+	record = {}
 
-	try:
-		with open(csv_file, newline='', encoding='utf-8') as csvfile:
-			reader = csv.reader(csvfile)
-			next(reader)
-			data = list(reader)
-		
-		OutlookEmailActiveUserList.objects.all().delete()
-		for item in data:
+	cred_file = path.abspath("temp\\cred.txt")
+	if path.exists(cred_file):
+		cred_file_is_existed = True
+	else:
+		cred_file_is_existed = False
 
-			if item[29] != "":
-				# print(item[29])
-				emp_id = Decimal(item[29])
+	if cred_file_is_existed:
+		try:
+			p = subprocess.Popen(["powershell.exe", "C:\\gfthboard\\temp\\email_script_1.ps1"], cwd="C:\\gfthboard\\temp", stdout=sys.stdout)
+			p.communicate()			
+
+			# Get latest updated date
+			latest_updated = OutlookEmailActiveUserList.objects.values_list('created_date', flat=True).first()
+
+			csv_file = path.abspath("media\\system\\export_outlook_365_email.csv")
+			if path.exists(csv_file):
+				csv_file_is_existed = True
 			else:
-				# print("blank")
-				emp_id = None
+				csv_file_is_existed = False
 
-			first_name = item[8]
-			last_name = item[10]
-			email = item[31]
+			if csv_file_is_existed:
+				try:
+					record = {}
+					with open(csv_file, newline='', encoding='utf-8') as csvfile:
+						reader = csv.reader(csvfile)
+						next(reader)
+						next(reader)
+						data = list(reader)
 
-			p = OutlookEmailActiveUserList(first_name=first_name, last_name=last_name, email=email, emp_id=emp_id)			
-			p.save()
-		
-		is_error = False
-		message = "Success"
+					OutlookEmailActiveUserList.objects.all().delete()
 
-	except Exception as e:
+					save_time = datetime.now()
+					
+					for item in data:
+						if item[2] != "" and item[2] is not None:
+							emp_id = Decimal(item[2])
+						else:
+							emp_id = None
+
+						first_name = item[0]
+						last_name = item[1]
+						email = item[4]
+
+						record = {
+							"first_name": first_name,
+							"last_name": last_name,
+							"email": email,							
+						}
+
+						office_365_email_list.append(record)
+						count = count + 1
+						outlook_obj = OutlookEmailActiveUserList(first_name=first_name, last_name=last_name, email=email, emp_id=emp_id, created_date=save_time)
+						outlook_obj.save()
+
+					# Get latest updated date
+					latest_updated = OutlookEmailActiveUserList.objects.values_list('created_date', flat=True).first()
+
+					is_error = False
+					message = "Success"
+
+				except Exception as e:
+					is_error = True
+					message = str(e)
+			else:
+				print("If csv file is not existed, do nothing.")
+				is_error = True
+				message = "Export file is not existed."
+
+		except subprocess.CalledProcessError as e:
+			is_error = True
+			message = str(e)
+
+	else:
 		is_error = True
-		message = str(e)
-
-
-	# TEST
-	'''
-	try:
-		sql = "select count(*) from employee;"
-		cursor = connections['reportdb'].cursor()
-		cursor.execute(sql)
-		leave_employeeinstance_object = cursor.fetchone()
-		error_message = "No error"
-	except db.OperationalError as e:
-		error_message = "<b>Error: please send this error to IT team</b><br>" + str(e)      
-	except db.Error as e:
-	    error_message = "<b>Error: please send this error to IT team</b><br>" + str(e)
-	finally:
-	    cursor.close()
-
-	if leave_employeeinstance_object is not None:
-		print("HRMS-Employee 51 : ", leave_employeeinstance_object[0])
-	else:
-		print("ERROR")
-	'''
-	# TEST
-
-
-	# TEST
-	'''
-	try:
-		sql = "select count(*) from employee;"
-		cursor = connections['hrms14'].cursor()
-		cursor.execute(sql)
-		leave_employeeinstance_object = cursor.fetchone()
-		error_message = "No error"
-	except db.OperationalError as e:
-		error_message = "<b>Error: please send this error to IT team</b><br>" + str(e)      
-	except db.Error as e:
-	    error_message = "<b>Error: please send this error to IT team</b><br>" + str(e)
-	except Exception as e:
-		error_message = str(e)
-	finally:
-	    cursor.close()
-
-	print(error_message)
-	'''
-
-	'''
-	if leave_employeeinstance_object is not None:
-		print("HRMS-Employee 14 : ", leave_employeeinstance_object[0])
-	else:
-		print("ERROR")
-	'''
-	# TEST
-
-
-
-	# from django.db import connections
-	# cursor = connections['reportdb'].cursor()
-	# cursor.execute("select * from leave_employeeinstance")
-	# cursor.close()
-
+		message = "ระบบไม่อนุญาติให้ดึงไฟล์จาก Office 365"
+	
 	response = JsonResponse(data={        
 		"is_error": is_error,
 		"message": message,
+		"latest_updated": latest_updated.strftime("%d/%m/%Y %H:%M:%S"),
+		"office_365_email_list": list(office_365_email_list),
+		"total": count,
 	})
 
 	response.status_code = 200
