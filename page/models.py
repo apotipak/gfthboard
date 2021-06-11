d1dc407f4fe8b7a838d932d26fabb3ca407c0f1a
Amnaj Potipak <amnaj.potipak@guardforce.co.th>
2021-06-11 12:49:29 +0700

    Add initial files.

--- page/models.py
+++ page/models.py
@@ -26,3 +26,28 @@ class ComDivision(models.Model):
 
     def __str__(self):
         return '{0}'.format(self.div_en)
+
+
+class CovidEmployeeVaccineUpdate(models.Model):    
+    emp_id = models.DecimalField(max_digits=7, decimal_places=0, blank=True, null=True)
+    full_name = models.CharField(max_length=200, blank=True, null=True)
+    
+    get_vaccine_count = models.DecimalField(max_digits=1, decimal_places=0, blank=True, null=True)
+    get_vaccine_date = models.DateTimeField(blank=True, null=True)
+    get_vaccine_place = upd_by = models.CharField(max_length=100, blank=True, null=True)    
+    file_attch = models.FileField(upload_to='documents/covid/', null=True)
+    file_attach_data = models.BinaryField(null=True)    
+
+    upd_date = models.DateTimeField(blank=True, null=True)
+    upd_by = models.CharField(max_length=10, blank=True, null=True)
+    upd_flag = models.CharField(max_length=1, blank=True, null=True)
+    
+
+    class Meta:
+        managed = True
+        db_table = 'covid_employee_vaccine_update'
+
+    '''
+    def __str__(self):
+        return '{0}'.format(self.div_en)
+    '''
