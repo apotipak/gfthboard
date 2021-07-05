3aff4702527212d4f480a8f482f4fc95b6170f63
gfthamnaj <60995426+gfthamnaj@users.noreply.github.com>
2021-07-06 00:03:03 +0700

    Dev (#441)
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files
    
    * Add initial files
    
    * Add initial files
    
    * Add initial files
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    * Add initial files.
    
    Co-authored-by: apotipak <apotipak@gmail.com>

--- epayslipm1/urls.py
+++ epayslipm1/urls.py
@@ -11,13 +11,10 @@ from django_otp.forms import OTPTokenForm
 
 
 urlpatterns = [
-    # path('', views.EPaySlipM1, name='e_payslip_m1'),	
-	url('', EPaySlipM1.as_view(), name='e_payslip_m1'),	
+    # path('', views.EPaySlipM1, name='e_payslip_m1'),		
     url(r'^ajax/send_pay_slip_m1/$', views.AjaxSendPayslipM1, name='ajax_send_pay_slip_m1'),
-    
-
     # url(r'^ajax/generate_payslip_m1/(?P<emp_id>\d+)/(?P<period>\w+)/$', views.generate_payslip_m1, name='generate_payslip_m1'),
-    
+    url('', EPaySlipM1.as_view(), name='e_payslip_m1'),    
 ]
 
 
